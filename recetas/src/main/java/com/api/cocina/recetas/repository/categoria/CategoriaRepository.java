8abfc798483c8ad61c58a17114b7a845d0765bdd
Nico Mega <nicolasmega01.08.94@gmail.com>
2026-01-23 19:12:40 -0300

    Agregar repository de Categoria

--- recetas/src/main/java/com/api/cocina/recetas/repository/categoria/CategoriaRepository.java
+++ recetas/src/main/java/com/api/cocina/recetas/repository/categoria/CategoriaRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.api.cocina.recetas.domain.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    
+    boolean existsByNombre(String nombre);
 
 }
