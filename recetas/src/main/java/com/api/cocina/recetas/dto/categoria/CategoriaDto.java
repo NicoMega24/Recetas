0a9352ca224573c4dd81d25b4ec6ff6e28812765
Nico Mega <nicolasmega01.08.94@gmail.com>
2026-01-23 19:08:28 -0300

    Agregar DTO de Categoria

---
+++ recetas/src/main/java/com/api/cocina/recetas/dto/categoria/CategoriaDto.java
@@ -0,0 +1,14 @@
+package com.api.cocina.recetas.dto.categoria;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoriaDto {
+
+    private Long id;
+    private String nombre;
+}
